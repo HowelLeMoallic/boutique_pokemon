d4a66dc16cac497e81bf794ae7dd6bf1919186a1
Howel <howel.lemoallic2022@campus-eni.fr>
2023-03-14 14:26:26 +0100

    Modification de la page d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,9 +6,12 @@ use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -18,6 +21,59 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('image', FileType::class, [
+                'label' => 'Image (format jpg ou png)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Un format jpg ou png est attendu',
+                    ])
+                ],
+            ])
+            ->add('prenomNom', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your first name and last name',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Your mail should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 255,
+                    ]),
+                ],
+                'required' => true,
+            ])
+            ->add('identifiant', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your identifiant',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Your mail should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 255,
+                    ]),
+                ],
+                'required' => true,
+            ])
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new NotBlank([
@@ -30,6 +86,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 255,
                     ]),
                 ],
+                'required' => true,
                 'data' => 'lemoallichowel@gmail.com',
             ])
             ->add('agreeTerms', CheckboxType::class, [
