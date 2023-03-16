eb94d4a84e7d9f51f18b724e39b7d065618a38ab
Howel <howel.lemoallic2022@campus-eni.fr>
2023-03-16 14:26:24 +0100

    Renommage du Controller Accueil en Article Transmission de l'identifiant de l'article entre la page index.html.twig et la page infoArticle.html.twig

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\ArticleRepository;
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+
+    public function __construct(private ArticleRepository $articleRepository, private CategorieRepository $categorieRepository)
+    {
+    }
+
+    #[Route('/article', name: 'app_accueil')]
+    public function index(): Response
+    {
+        $articles = $this->articleRepository->findAll();
+
+        $categories = $this->categorieRepository->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'article_info')]
+    public function infoArticle($id){
+
+        $article = $this->articleRepository->find($id);
+
+        if ($article){
+
+            return $this->render('article/infoArticle.html.twig', [
+                'article' => $article
+            ]);
+        }
+
+        $this->addFlash('error', 'Cet article n\'existe pas');
+
+        $this->addFlash('error', 'Vous n\'avez pas accès à cette sortie' );
+
+        return $this->redirectToRoute('app_accueil');
+
+    }
+}
